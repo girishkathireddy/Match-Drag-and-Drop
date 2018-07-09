9edfcfa36d551bd075fffda74985b7123dd5db1a
Monicamrsty <monica.mrsty@gmail.com>
2018-07-09 17:07:06 -0400

    Modified UI. Added Jquey UI effects for Drop Boxes

--- js/script.js
+++ js/script.js
@@ -10,9 +10,12 @@ $(function(){
             cancel: false,
             revert: function (event, ui) {
                 if(!event){
-                    $('#section-drop').css({'filter': 'blur(5px)'});
-                    $("#dialog-message").dialog("open");
+                    // dialg box
+                    // $('#section-drop').css({'filter': 'blur(5px)'});
+                    // $("#dialog-message").dialog("open");
                     // comment section
+                    $('#comment-section').text('Please try once again');
+                    $('#comment-section').removeClass('comment-success')
                     $('#comment-section').css({"opacity":"1"});
                     $('#comment-section').addClass('comment-sec')
                 }
@@ -40,9 +43,11 @@ $(function(){
             $(this).css({'background-color':'rgb(5, 107, 98)'});
             $(this).find('h2').css({'color':'#fff'});
             $(this).append($(ui.draggable).clone());
-            $(this).effect( "shake", { times:2 }, 300);
-            // console.log($(this).parent());
-            // console.log(  $(this).find('h2').css({'background-color':'#4e5a59'}));
+            //shake effect
+            // $(this).effect( "shake", { times:2 }, 300);
+
+            // display Comment success
+            displayCommentSuccess();
 
             var IDs = [];
             $("#droppable-items").find("button").each(function(){ IDs.push(this.id); });
@@ -59,7 +64,8 @@ $(function(){
     });
 
 
-    $("#auxilary-verb").droppable({
+    // auxilary-verb
+        $("#auxilary-verb").droppable({
         accept: function(d) {
             return true;
         },
@@ -75,9 +81,13 @@ $(function(){
         },
         drop: function(evt, ui) {
             $(this).css({'background-color':'rgb(5, 107, 98)'});
-            $(this).append($(ui.draggable).clone());
             $(this).find('h2').css({'color':'#fff'});
-            console.log($(this).html());
+            $(this).append($(ui.draggable).clone());
+            //shake effect
+            // $(this).effect( "shake", { times:2 }, 300);
+
+            // display Comment success
+            displayCommentSuccess();
 
             var IDs = [];
             $("#droppable-items").find("button").each(function(){ IDs.push(this.id); });
@@ -88,9 +98,11 @@ $(function(){
                 $('#'+IDs[i]).css({'color':'rgba(153, 153, 153, 0.6)'});
                 $('#'+IDs[i]).css({"background-color":"#fff"});
                 $('#'+IDs[i]).css({ "pointer-events": "none"});
+
             }
         }
     });
+
 
     // Main verb
     $("#main-verb").droppable({
@@ -111,6 +123,8 @@ $(function(){
             $(this).css({'background-color':'rgb(5, 107, 98)'});
             $(this).find('h2').css({'color':'#fff'});
             $(this).append($(ui.draggable).clone());
+            $()
+
             console.log(  $(this).find('h2').css({'background-color':'#4e5a59'}));
 
 
@@ -149,30 +163,38 @@ $(function(){
         $("#butns-area").append(input);
     }
 
+    // function to display comments
+ function displayCommentSuccess(){
+     $('#comment-section').removeClass('comment-sec');
+     $('#comment-section').addClass('comment-success');
+     $('#comment-section').text('Success');
+     $('#comment-section').css({"opacity":"1"});
+ }
+
 
     // Dialog box for comments
-    $("#dialog-message").dialog({
-        autoOpen : false,
-        resizable : false,
-        width : 200,
-        height:200,
-        modal : true,
-        buttons: {
-            "Close" : function(){
-                $( this ).dialog( "close" );
-            }
-        },
-        open: function(event) {
-            $('.ui-dialog-content.ui-widget-content').css({'height':'auto'});
-            $('.ui-dialog-buttonpane').css({'padding':'0'});
-            $('.ui-dialog-buttonpane .ui-dialog-buttonset').css({'float':'unset'});
-            $('.ui-dialog-buttonpane').find('button:contains("Close")').addClass('closeButton');
-        },
-        close : function(ev, ui) {
-            $('#section-drop').css({'filter': 'blur(0px)'});
-            return true;
-        }
-    });
+    // $("#dialog-message").dialog({
+    //     autoOpen : false,
+    //     resizable : false,
+    //     width : 200,
+    //     height:200,
+    //     modal : true,
+    //     buttons: {
+    //         "Close" : function(){
+    //             $( this ).dialog( "close" );
+    //         }
+    //     },
+    //     open: function(event) {
+    //         $('.ui-dialog-content.ui-widget-content').css({'height':'auto'});
+    //         $('.ui-dialog-buttonpane').css({'padding':'0'});
+    //         $('.ui-dialog-buttonpane .ui-dialog-buttonset').css({'float':'unset'});
+    //         $('.ui-dialog-buttonpane').find('button:contains("Close")').addClass('closeButton');
+    //     },
+    //     close : function(ev, ui) {
+    //         $('#section-drop').css({'filter': 'blur(0px)'});
+    //         return true;
+    //     }
+    // });
 
 
 
